a5f3a2af7a47cc7cbdbe2f88204ac0540389d141
Shiv Arora <shiv.arora@deeplogictech.com>
2025-02-06 15:26:38 +0530

    Added the functions

---
+++ expense-tracker.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const { Command } = require("commander");
+const program = new Command();
+
+program.name("extracker")
+.description("A CLI expense tracker tool")
+.version("1.0.0");
+
+const DATA_FILE = path.join(__dirname,'expenses.json');
+
+function loadExpenses(){
+    if(!fs.existsSync(DATA_FILE)){
+        return [];
+    }
+    try{
+        return JSON.parse(fs.readFileSync(DATA_FILE,'utf-8'));
+    } catch(err){
+        return [];
+    }
+}
+function saveExpenses(expenses){
+    fs.writeFileSync(DATA_FILE,JSON.stringify(expenses,null,2));
+}
+
+function getNextId(expenses){
+    if(expenses.length===0) return 1;
+    return Math.max(...expenses.map(exp=>exp.id))+1;
+}
+
+
+
